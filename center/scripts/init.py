7de0f9c9174f66b845ccfff1397cff867f98525c
Eli Baranskiy <elibj123@gmail.com>
2020-04-15 21:59:32 +0300

    - add help for init.py script - for windows all services start in a separate shell by default unless -sw flag is used in command line

--- center/scripts/init.py
+++ center/scripts/init.py
@@ -5,21 +5,32 @@ import shutil
 import json
 import platform
 
-
+sysname = platform.system().lower()
 
 env_file = 'public'
 services_to_run = None
+separate_window = True if sysname == 'windows' else False
 i = 0 
 while i < len(sys.argv):
-    if sys.argv[i] == '-e':
+    if sys.argv[i] == 'help':
+        print('use to start required services for netSens')
+        print('usage: python init.py [-e env_file_name] [-s service1,service2,...] [-nw]')
+        print('-e env_file_name - sets the desired env file for the web service')
+        print('-s service1,service2,... - loads only specified services from the service.json file. If not specified all services are loaded by default')
+        print('-nw - supported only in windows. If used, all services will be run within the same shell window.')
+        sys.exit(0)
+    elif sys.argv[i] == '-e':
         env_file = sys.argv[i+1]
         i += 1
     elif sys.argv[i] == '-s':
         services_to_run = sys.argv[i+1].split(',')
         i += 1
+    elif sys.argv[i] == '-nw':
+        if sysname == 'windows':
+            separate_window = False
     i += 1
 
-service_file = f'services.{platform.system().lower()}.json' 
+service_file = f'services.{sysname}.json' 
 with open(service_file, 'r') as fp:
     services = json.load(fp)
 
@@ -63,5 +74,7 @@ for service in services:
     print('starting service: %s' % service['name'])
     os.chdir(service['execPath'])
     cmd = service['cmd'].replace('%rd%', root_dir).split(' ')
+    if separate_window:
+        cmd.insert(0,'start')
     subprocess.Popen(cmd, shell=True)
     os.chdir(root_dir)
