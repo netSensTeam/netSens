cf8700d93897ae127e3579a8e296776e96ff6b36
Eli Baranskiy <elibj123@gmail.com>
2020-04-15 21:48:04 +0300

    - add command line options to init script - allow user to select which services are run

--- center/scripts/init.py
+++ center/scripts/init.py
@@ -5,10 +5,32 @@ import shutil
 import json
 import platform
 
-service_file = "services.%s.json" % platform.system().lower()
+
+
+env_file = 'public'
+services_to_run = None
+i = 0 
+while i < len(sys.argv):
+    if sys.argv[i] == '-e':
+        env_file = sys.argv[i+1]
+        i += 1
+    elif sys.argv[i] == '-s':
+        services_to_run = sys.argv[i+1].split(',')
+        i += 1
+    i += 1
+
+service_file = f'services.{platform.system().lower()}.json' 
 with open(service_file, 'r') as fp:
-	services = json.load(fp)
+    services = json.load(fp)
+
 os.chdir('..')
+env_path = f'app/web/env_{env_file}.py'
+if not os.path.exists(env_path):
+    raise Exception(f'ENV file {env_file} not found')
+    sys.exit(0)
+    
+shutil.copyfile(env_path, 'app/web/env.py')
+
 root_dir = os.getcwd()
 dirlist = [
 	'data',
@@ -28,26 +50,18 @@ dirlist = [
 ]
 
 for d in dirlist:
-	dir = os.path.join(root_dir, d)
-	if not os.path.exists(dir):
-		print('creating dir: %s' % dir)
-		os.mkdir(dir)
-
-# prepare env file for web.py
-if len(sys.argv) == 1:
-	env_file = 'public'
-else:
-	env_file = sys.argv[1]
-
-shutil.copyfile('app/web/env_%s.py' % env_file, 'app/web/env.py')
-
-
+    dir = os.path.join(root_dir, d)
+    if not os.path.exists(dir):
+        print(f'creating dir: {dir}')
+        os.mkdir(dir)
 
 for service in services:
-	if 'active' in service and not service['active']:
-		continue
-	print('starting service: %s' % service['name'])
-	os.chdir(service['execPath'])
-	cmd = service['cmd'].replace('%rd%', root_dir).split(' ')
-	subprocess.Popen(cmd, shell=True)
-	os.chdir(root_dir)
+    if services_to_run and service['name'] not in services_to_run:
+        continue
+    if 'active' in service and not service['active']:
+        continue
+    print('starting service: %s' % service['name'])
+    os.chdir(service['execPath'])
+    cmd = service['cmd'].replace('%rd%', root_dir).split(' ')
+    subprocess.Popen(cmd, shell=True)
+    os.chdir(root_dir)
